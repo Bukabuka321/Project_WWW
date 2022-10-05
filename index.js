2f8a2333208850328f7361a4bcb039824f2aeb34
OLGA DOBROVOLSKA <3250186@gmail.com>
2022-10-05 22:22:01 +0300

    renaming images, adding styles of objects

--- index.js
+++ index.js
@@ -1,41 +1,41 @@
-const AboutUs = document.querySelector(".AboutUs");
-class cardAboutUs {
+const aboutUs = document.querySelector(".AboutUs");
+class CardAboutUs {
     constructor(title, picture, text) {
         this.title = title;
         this.picture = picture;
         this.text = text;
     }
 }
-const WhoWeAre = new cardAboutUs("Who we are", "emoji_who we.png", "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation");
-const WhatWeDo = new cardAboutUs("What we do", "emoji_hacker cat.png", "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation");
-const OurGoals = new cardAboutUs("Our goals", "emoji_goals.png", "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation");
+const whoWeAre = new CardAboutUs("Who we are", "emoji _who we.png", "Lorem ipsum dolor sit amet, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation");
+const whatWeDo = new CardAboutUs("What we do", "emoji _hacker cat_.png", "Lorem ipsum dolor sit amet, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation");
+const ourGoals = new CardAboutUs("Our goals", "emoji _goals.png", "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation");
 
-function createCard(title, picture, text) {
+const cards = [whoWeAre, whatWeDo, ourGoals];
+
+function createCard(objCard) {
     const card = document.createElement("div");
-    card.classList.add("cardAboutUs");
-    AboutUs.appendChild(card);
+    card.classList.add("CardAboutUs");
+    aboutUs.appendChild(card);
 
     const cardHeader = document.createElement("div");
     cardHeader.classList.add("card-header");
-    cardAboutUs.appendChild(cardHeader);
+    card.appendChild(cardHeader);
 
     const cardTitle = document.createElement("h1");
     cardTitle.classList.add("card-title");
-    cardTitle.innerHTML = `${title}`;
+    cardTitle.innerHTML = `${objCard.title}`;
     cardHeader.appendChild(cardTitle);
 
-    const cardImg = document.createElement("div");
-    cardImg.src = `${picture}`;
+    const cardImg = document.createElement("img");
+    cardImg.src = `assets/${objCard.picture}`;
     cardImg.classList.add("card-img");
     cardHeader.appendChild(cardImg);
 
-    const cardBody = document.createElement("div");
+    const cardBody = document.createElement("p");
     cardBody.classList.add("card-body");
-    cardBody.innerHTML = `${text}`;
-    cardAboutUs.appendChild(cardBody);
-
+    cardBody.innerHTML = `${objCard.text}`;
+    card.appendChild(cardBody);
 }
-
-createCard(WhoWeAre);
-createCard(WhatWeDo);
-createCard(OurGoals);
+cards.forEach((cardObj) => {
+    createCard(cardObj);
+})
